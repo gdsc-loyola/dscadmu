d21a888832932ec732826e7e25029570249c2bd0
Krizza Bullecer <krizzabullecer@gmail.com>
2020-03-21 02:22:31 +0800

    rename components accordingly and initialize event/mission cards

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "image"]
+};
+</script>
+
+<style scoped>
+div {
+  box-shadow: 2px 4px 2px #e0e0e0;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
