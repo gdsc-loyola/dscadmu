1eaec8d33de368d07e09d3b1e51d1dc997382610
Krizza Bullecer <krizzabullecer@gmail.com>
2020-03-21 02:25:42 +0800

    initialize in progress page for incomplete routes

---
+++ src/views/InProgress.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Finishing the code soon</h1>
+    <p>
+      Developer Student Clubs, a program under the Google Developers, is an
+      international community of students from various chapters who share a
+      common interest in technology. Google brought DSC to Southeast Asia last
+      year. Now, it's coming to the Philippines!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  height: 100vh;
+}
+</style>
