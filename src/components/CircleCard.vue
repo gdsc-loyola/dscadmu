3c6a8d53eb970db0859ba22dc4db134c102eab7e
Krizza Bullecer <krizzabullecer@gmail.com>
2020-03-25 02:00:32 +0800

    replace static code with props

--- src/components/CircleCard.vue
+++ src/components/CircleCard.vue
@@ -1,22 +1,36 @@
 <template>
   <div>
-    <img src="circle.jpg" alt="photo" />
-    <h6>Name of Chapter</h6>
-    <p>Manila City, Luzon</p>
+    <img
+      :src="require('@/assets/images/' + image + '')"
+      alt="DSC Chapter photo"
+    />
+    <h6>{{ title }}</h6>
+    <p>{{ location }}</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["image", "title", "location"]
+};
 </script>
 
 <style scoped>
 div {
+  flex: 0 0 auto;
   margin: 0 26px;
   width: 192px;
+  text-align: center;
 }
 
 img {
   margin-bottom: 24px;
+  width: 100%;
+  border-radius: 100%;
+}
+
+h6 {
+  line-height: 25px;
+  margin-bottom: 4px;
 }
 </style>
