9607f3f3436ff968cbb18c9a29e8a6a913f36b52
Krizza Bullecer <krizzabullecer@gmail.com>
2020-03-24 14:34:55 +0800

    add mobile responsive styles

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,33 +1,38 @@
 <template>
   <div>
-    <section class="grey-bg">
-      <div class="container" id="hero">
-        <div>
-          <h4>Vision</h4>
-          <h1>Uplifting communities through technology</h1>
-          <p class="feature-text">
-            We are a community of developers that are passionate about uplifting
-            communities through technology and innovation.
-          </p>
-          <button class="blue">Join us</button>
-        </div>
-        <div>
-          <img src="../assets/images/feature-image.svg" alt="Feature image" />
+    <section id="hero">
+      <div class="container">
+        <h4>Vision</h4>
+        <div class="row align-items-center justify-content-between">
+          <div id="hero-text" class="col-12 col-md-6">
+            <h1>Uplifting communities through technology</h1>
+            <p class="feature-text">
+              We are a community of developers that are passionate about
+              uplifting communities through technology and innovation.
+            </p>
+            <router-link to="/">
+              <button class="blue">Join us</button>
+            </router-link>
+          </div>
+          <div id="hero-image" class="col-12 col-md-6">
+            <img src="../assets/images/feature-image.svg" alt="Feature image" />
+          </div>
         </div>
       </div>
     </section>
-    <section id="mission">
+
+    <section id="mission" class="grey-bg">
       <div class="container">
         <h2 class="text-center">Our mission</h2>
-        <div id="mission-list">
-          <mission-card image="empower.svg" title="Empower"
-            >We empower people through technology and programming
-            education</mission-card
-          >
-          <mission-card image="enlighten.svg" title="Enlighten"
-            >We enlighten people to the power of innovation and
-            problem-solving</mission-card
-          >
+        <div class="row justify-content-center">
+          <mission-card image="empower.svg" title="Empower">
+            We empower people through technology and programming
+            education
+          </mission-card>
+          <mission-card image="enlighten.svg" title="Enlighten">
+            We enlighten people to the power of innovation and
+            problem-solving
+          </mission-card>
           <mission-card image="social-care.svg" title="Nurture">
             We nurture people to create meaningful technological solutions for
             the community
@@ -35,40 +40,74 @@
         </div>
       </div>
     </section>
-    <section class="grey-bg">
-      <div class="container">
-        <h2 class="text-center">Types of events</h2>
-        <p class="text-center">
-          Various events to empower, enlighten, and nurture members
-        </p>
-        <div class="event-list">
-          <event-card></event-card>
+
+    <section id="event">
+      <div class="container text-center">
+        <h2>Types of events</h2>
+        <p>Various events to empower, enlighten, and nurture members</p>
+        <div class="row">
+          <router-link class="col-6 col-md-6 col-lg-3" to="/">
+            <event-card image="study-jams.jpeg">Study Jams</event-card>
+          </router-link>
+          <router-link class="col-6 col-md-6 col-lg-3" to="/">
+            <event-card image="workshop.jpeg">Hands-on Workshops</event-card>
+          </router-link>
+          <router-link class="col-6 col-md-6 col-lg-3" to="/">
+            <event-card image="exploreml.jpeg">ExploreML</event-card>
+          </router-link>
+          <router-link class="col-6 col-md-6 col-lg-3" to="/">
+            <event-card image="hackathon.jpeg">Hackathon</event-card>
+          </router-link>
+        </div>
+        <div class="row">
+          <router-link class="col-6 col-md-6 col-lg-4" to="/">
+            <event-card image="talk.jpeg">Tech Talk</event-card>
+          </router-link>
+          <router-link class="col-6 col-md-6 col-lg-4" to="/">
+            <event-card image="bootcamp.jpeg">Bootcamp</event-card>
+          </router-link>
+          <router-link class="col-6 col-md-6 col-lg-4" to="/">
+            <event-card image="showcase.jpeg">Showcase</event-card>
+          </router-link>
         </div>
       </div>
     </section>
-    <section>
+
+    <section id="tech" class="grey-bg">
       <div class="container">
-        <h2>Google Technologies</h2>
-        <p>
+        <h2 class="text-center">Google Technologies</h2>
+        <p class="text-center">
           We’re building solutions by learning and applying Google’s latest
           technologies.
         </p>
-        <div id="tech">
-          <img src="../assets/images/angular.svg" alt="Angular logo" />
-          <img src="../assets/images/android.svg" alt="Android logo" />
-          <img
-            src="../assets/images/actions-on-google.svg"
-            alt="Actions on Google logo"
-          />
-          <img src="../assets/images/dialogflow.svg" alt="Dialogflow logo" />
-          <img src="../assets/images/google-apps.svg" alt="Google Apps logo" />
-          <img src="../assets/images/tensorflow.svg" alt="TensorFlow logo" />
-          <img
-            src="../assets/images/google-cloud-platform.svg"
-            alt="Google Cloud Platform logo"
-          />
-          <img src="../assets/images/flutter.svg" alt="Flutter logo" />
-          <img src="../assets/images/firebase.svg" alt="Firebase logo" />
+        <div class="row align-items-center text-center">
+          <div class="col-4 col-md-4 col-lg">
+            <img src="../assets/images/angular.svg" alt="Angular logo" />
+          </div>
+          <div class="col-4 col-md-4 col-lg">
+            <img src="../assets/images/google-apps.svg" alt="Google Apps logo" />
+          </div>
+          <div class="col-4 col-md-4 col-lg">
+            <img src="../assets/images/android.svg" alt="Android logo" />
+          </div>
+          <div class="col-4 col-md-4 col-lg">
+            <img src="../assets/images/actions-on-google.svg" alt="Actions on Google logo" />
+          </div>
+          <div class="col-4 col-md-4 col-lg">
+            <img src="../assets/images/dialogflow.svg" alt="Dialogflow logo" />
+          </div>
+          <div class="col-4 col-md-4 col-lg">
+            <img src="../assets/images/tensorflow.svg" alt="TensorFlow logo" />
+          </div>
+          <div class="col-4 col-md-4 col-lg">
+            <img src="../assets/images/flutter.svg" alt="Flutter logo" />
+          </div>
+          <div class="col-4 col-md-4 col-lg">
+            <img src="../assets/images/google-cloud-platform.svg" alt="Google Cloud Platform logo" />
+          </div>
+          <div class="col-4 col-md-4 col-lg">
+            <img src="../assets/images/firebase.svg" alt="Firebase logo" />
+          </div>
         </div>
       </div>
     </section>
@@ -89,14 +128,8 @@ export default {
 
 <style scoped>
 #hero {
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-#hero h4 {
-  margin-bottom: 25px;
+  margin-top: 55px;
+  width: 100%;
 }
 
 #hero p {
@@ -107,22 +140,53 @@ export default {
   width: 100%;
 }
 
-#mission h2 {
+#hero h4 {
+  color: #4285f4;
+}
+
+#hero-text {
+  max-width: 500px;
+}
+
+#event p,
+#mission h2,
+#tech p {
   margin-bottom: 50px;
 }
 
-#mission-list {
-  display: flex;
-  justify-content: center;
-}
+@media screen and (max-width: 768px) {
+  #hero p {
+    margin-top: 1rem;
+  }
 
-#tech {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+  #hero-image {
+    order: -1;
+  }
 
-#tech img {
-  margin: 30px 16px 0 16px;
+  #mission {
+    padding-bottom: 20px;
+  }
+
+  #event a {
+    padding: 0 8px;
+  }
+
+  #event p,
+  #mission h2,
+  #tech p {
+    margin-bottom: 24px;
+  }
+
+  #tech {
+    padding-bottom: 30px;
+  }
+
+  #tech .col-lg {
+    margin-bottom: 30px;
+  }
+
+  #tech img {
+    width: 100%;
+  }
 }
 </style>
