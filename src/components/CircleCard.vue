603eb1c4dc398c1ab0dc0181a7af087ce0bc5259
Krizza Bullecer <krizzabullecer@gmail.com>
2020-03-25 01:25:56 +0800

    initialize components

---
+++ src/components/CircleCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img src="circle.jpg" alt="photo" />
+    <h6>Name of Chapter</h6>
+    <p>Manila City, Luzon</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  margin: 0 26px;
+  width: 192px;
+}
+
+img {
+  margin-bottom: 24px;
+}
+</style>
