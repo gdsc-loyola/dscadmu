4484d161f1e8b53aa897691170cecf9aa7b79804
Krizza Bullecer <krizzabullecer@gmail.com>
2020-03-24 17:42:23 +0800

    initialize not found component

---
+++ src/views/NotFound.vue
@@ -0,0 +1,31 @@
+<template>
+  <section id="wip" class="grey-bg">
+    <div class="container">
+      <h4>Oh no!</h4>
+      <h1>Page Not Found</h1>
+      <p class="feature-text">
+        Looks like the page is gone! Either the link is broken or the page has
+        already been removed!
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  mounted() {
+    window.scrollTo(0, 0);
+  }
+};
+</script>
+
+<style scoped>
+#wip {
+  margin-top: 55px;
+  width: 100%;
+}
+
+#wip h4 {
+  color: #ea4335;
+}
+</style>
