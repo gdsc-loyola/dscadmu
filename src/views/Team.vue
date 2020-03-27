fe25f2b896239376f480ab2fa6b60b4467661ec8
Krizza Bullecer <krizzabullecer@gmail.com>
2020-03-27 17:02:13 +0800

    initialize view

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,9 +1,58 @@
 <template>
-  <div></div>
+  <div>
+    <section id="leader" class="grey-bg">
+      <div class="container">
+        <h2>Meet our leadership team</h2>
+        <p class="feature-text">
+          Our lineup that empowers, enlightens, and nurtures people around the
+          world.
+        </p>
+        <h4>Executive Board & Co-Founders</h4>
+        <div class="row">
+          <team-circle
+            name="Harvey Sison"
+            fb="harveymile"
+            link="harveyjaysison"
+            git="Hjkun77"
+            image="person.png"
+          >
+            President/CEO,
+            <br />Co-Founder & DSC Lead
+          </team-circle>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {};
+import TeamCircle from "@/components/TeamCircle";
+
+export default {
+  created() {
+    window.onresize = this.handleResize;
+  },
+  components: {
+    TeamCircle
+  },
+  methods: {
+    handleResize() {}
+  }
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+#leader {
+  margin-top: 60px;
+}
+
+#leader h2,
+#leader h4 {
+  text-align: center;
+}
+
+#leader h4 {
+  margin: 48px 0 32px;
+  color: #0f9d58;
+}
+</style>
