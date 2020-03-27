6902716ab99e183b7b6f54444f6bb4130c1a7e43
Krizza Bullecer <krizzabullecer@gmail.com>
2020-03-28 02:51:03 +0800

    set up structure of view

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -4,6 +4,21 @@
       <div>1</div>
       <div>2</div>
     </SlickCarousel>
+    <section class="grey-bg">
+      <div class="container">
+        <h2>Study Jams</h2>
+        <p>Community-run study groups for developers</p>
+        <div></div>
+
+        <h2>Hands-on Workshops</h2>
+        <p>Opportunity for community members to learn something new in tech</p>
+        <div></div>
+
+        <h2>Speaker Session & Tech Talk</h2>
+        <p>Community-run study groups for developers</p>
+        <div></div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -22,5 +37,9 @@ export default {
 <style scoped>
 div.slick-slider {
   margin-top: 60px;
+}
+
+h2 {
+  margin-top: 64px;
 }
 </style>
