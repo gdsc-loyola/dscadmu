121782bf27d2e87c9610b8fbca1e63ffe0482b63
Krizza Bullecer <krizzabullecer@gmail.com>
2020-03-28 02:45:38 +0800

    initialize carousel component

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div>
+    <SlickCarousel :arrows="false" :dots="true">
+      <div>1</div>
+      <div>2</div>
+    </SlickCarousel>
+  </div>
 </template>
 
 <script>
-export default {};
+import SlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+
+export default {
+  components: {
+    SlickCarousel
+  }
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+div.slick-slider {
+  margin-top: 60px;
+}
+</style>
