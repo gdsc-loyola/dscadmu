dcc81c52b51e18b1b7f37d20e4a01c11c8745fcd
Krizza Bullecer <krizzabullecer@gmail.com>
2020-03-29 11:50:18 +0800

    use vue dragscroll

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueDragscroll from "vue-dragscroll";
 import "@/assets/bootstrap-grid.css";
 
 Vue.config.productionTip = false;
+Vue.use(VueDragscroll);
 
 new Vue({
   router,
