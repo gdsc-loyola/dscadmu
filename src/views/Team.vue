0cfe53b0ee94c75950171ee1468520cd4bb33d5b
Hjkun77 <harveyjaysison@gmail.com>
2020-05-30 13:19:39 +0800

    fix hr duplicate

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -274,7 +274,7 @@ export default {
     window.removeEventListener("resize", this.handleResizeTeam);
   },
   components: {
-    TeamCircle,
+    TeamCircle
   },
   data() {
     return {
@@ -283,14 +283,10 @@ export default {
         window.innerWidth >= 768 ? "Meet our leadership team" : "Meet our team",
       ops: {
         backgroundImage:
-          "url(" + require("@/assets/images/operations.png") + ")",
+          "url(" + require("@/assets/images/operations.png") + ")"
       },
       tech: {
-        backgroundImage: "url(" + require("@/assets/images/tech.png") + ")",
-      },
-      hr: {
-        backgroundImage:
-          "url(" + require("@/assets/images/human-resources.png") + ")",
+        backgroundImage: "url(" + require("@/assets/images/tech.png") + ")"
       },
       hr: {
         backgroundImage:
@@ -315,8 +311,8 @@ export default {
         this.isDesktop = false;
         this.leaderHead = "Meet our leadership team";
       }
-    },
-  },
+    }
+  }
 };
 </script>
 
