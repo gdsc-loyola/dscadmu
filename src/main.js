0fc8c5737ba47078ff8881f88f6a819236d1229b
Hjkun77 <harveyjaysison@gmail.com>
2020-06-06 00:08:20 +0800

    vuefire setup

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import router from "./router";
 import VueDragscroll from "vue-dragscroll";
 import VModal from "vue-js-modal";
 import "@/assets/bootstrap-grid.css";
+import VueFire from "vuefire";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+Vue.use(VueFire);
+firebase.initializeApp({
+  projectId: "dscadmu",
+  databaseURL: "https://dscadmu.firebaseio.com"
+});
 
 Vue.config.productionTip = false;
 Vue.use(VueDragscroll);
@@ -13,3 +22,5 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
+
+export const db = firebase.firestore();
